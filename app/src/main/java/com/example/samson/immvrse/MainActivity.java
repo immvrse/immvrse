910231411a42c68c04b84f9d18bd267892b4be80
cheurfi <samira.cheurfi@gmail.com>
2018-01-25 16:05:56 +0000

    login screen UI

---
+++ app/src/main/java/com/example/samson/immvrse/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.samson.immvrse;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.samson.immvrse.databinding.ActivityMainBinding;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        binding.signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, OnBoarding.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/samson/immvrse/OnBoarding.java
@@ -0,0 +1,13 @@
+package com.example.samson.immvrse;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class OnBoarding extends Activity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_onboarding);
+    }
+}
