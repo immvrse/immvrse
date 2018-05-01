121e8013104868eda1e6664c52616da0aaa93946
cheurfi <samira.cheurfi@gmail.com>
2018-05-01 12:43:55 +0000

    changes to video loader

--- app/src/main/java/com/example/samson/immvrse/NewsFeedActivity.java
+++ app/src/main/java/com/example/samson/immvrse/NewsFeedActivity.java
@@ -1,6 +1,8 @@
 package com.example.samson.immvrse;
 
-import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -12,19 +14,20 @@ import com.example.samson.immvrse.databinding.ActivityNewsFeedBinding;
 
 public class NewsFeedActivity extends FragmentActivity {
 
-    private ActivityNewsFeedBinding binding;
+    ActivityNewsFeedBinding binding;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_feed);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_news_feed);
 
-        binding.menuButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(NewsFeedActivity.this, MenuNavigationActivity.class);
-                startActivity(intent);
-            }
-        });
+//        binding.menuButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(NewsFeedActivity.this, MenuNavigationActivity.class);
+//                startActivity(intent);
+//            }
+//
     }
 }
--- app/src/main/java/com/example/samson/immvrse/NewsFeedFragment.java
+++ app/src/main/java/com/example/samson/immvrse/NewsFeedFragment.java
@@ -1,19 +1,19 @@
 package com.example.samson.immvrse;
 
-import android.app.Fragment;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.samson.immvrse.databinding.FragmentNewsFeedBinding;
 
-public class NewsFeedFragment extends Fragment implements View.OnClickListener {
+public class NewsFeedFragment extends Fragment {
 
-    private FragmentNewsFeedBinding binding;
+    FragmentNewsFeedBinding binding;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,17 +23,15 @@ public class NewsFeedFragment extends Fragment implements View.OnClickListener {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        FragmentNewsFeedBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_news_feed, container, false);
-        return binding.getRoot();
-    }
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_news_feed, container, false);
 
-    @Override
-    public void onClick(View view) {
         binding.newsFeedImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getActivity(), VideoViewerActivity.class);
+                startActivity(intent);
             }
         });
+        return binding.getRoot();
     }
 }
